862aa3e6e1bfff3d60067190d0f57b0ce61c0582
LÊ HOÀNG THÂN <162953480+Le-Hoang-Than@users.noreply.github.com>
2026-04-16 14:50:03 +0700

    Update AppServiceProvider.php

--- he-thong-thi-trac-nghiem-client/app/Providers/AppServiceProvider.php
+++ he-thong-thi-trac-nghiem-client/app/Providers/AppServiceProvider.php
@@ -3,17 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema; 
+use Illuminate\Support\Facades\URL; // <--- 1. THÊM DÒNG NÀY
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Schema::defaultStringLength(191); 
+      
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
