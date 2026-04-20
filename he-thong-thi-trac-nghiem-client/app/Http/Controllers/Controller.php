9662db7b70883101b4a74714f8d39866a9d7a871
ChisPhong <chiphongle389@gmail.com>
2026-04-20 10:06:58 +0700

    Update Controller.php

--- he-thong-thi-trac-nghiem-client/app/Http/Controllers/Controller.php
+++ he-thong-thi-trac-nghiem-client/app/Http/Controllers/Controller.php
@@ -4,12 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    protected $api;
     protected $apiUrl;
 
     public function __construct()
     {
-        $this->api = config('app.base_api');
-        $this->apiUrl = $this->api;
+        $this->apiUrl = rtrim(env('BASE_API_URL', 'https://he-thong-thi-trac-nghiem-service-lnup.onrender.com'), '/');
     }
 }
